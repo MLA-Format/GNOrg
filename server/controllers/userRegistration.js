23d8b2691a1efa3c26b8ba76421aadaadd77140c
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-12 18:11:25 -0400

    Now references actual mongodb url

--- server/controllers/userRegistration.js
+++ server/controllers/userRegistration.js
@@ -1,9 +1,11 @@
 const { MongoClient } = require('mongodb');
-const dbUrl = "";
+const dbUrl = process.env.MONGODB_URL;
 
 
 
 async function registerUser()
 {
+    const {name, password, email} = req.body;
 
+    
 }
