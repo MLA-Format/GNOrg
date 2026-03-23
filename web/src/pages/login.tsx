ae34f3d9069a4ab2915ab37346483b458535e6a9
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-23 15:42:15 -0400

    Fixed New Line Entry, added login button, and added handleSubmit function

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -3,14 +3,21 @@ import NewLineEntry from '../components/NewLineEntry.tsx'
 import { Link } from 'react-router-dom';
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async () => { }
+
     return (
         <div>
             <h1>User login</h1>
             <p>Username</p>
-            <NewLineEntry title="username" />
+            <NewLineEntry title="username" value={username} onChange={(e) => setUsername(e.target.value)} />
             <p>Password</p>
-            <NewLineEntry title="password" />
-            <button>Login</button>
+            <NewLineEntry title="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {error && <p style={{color: "red"}}>{error}</p>}
+            <button onClick={handleSubmit}>Login</button>
             <Link to="/register">Don't have an account?</Link>
         </div>
     );
