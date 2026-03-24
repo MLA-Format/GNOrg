f7d6ef3d11e53ee925adc1fd2ad0bde98caab47e
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-24 19:09:44 -0400

    Added insert game api func

---
+++ server/db/games.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGODB_URL);
+const db = () => client.db("GNOrgDB").collection("games");
+
+const connect = async () => await client.connect();
+
+const insertGame = async (game) => db().insertOne({
+  name: game.name,
+  players: game.players || null,
+  genre: {
+    category: game.genre?.category || null,
+    type: game.genre?.type || null,
+  },
+  portable: game.portable ?? null,
+  coverImage: game.coverImage || null,
+});
+
+module.exports = { connect, insertGame };
