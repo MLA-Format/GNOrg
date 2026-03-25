4574c0ffb86fe448a41ece5f8b0ca10b17b2bbae
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-24 20:37:41 -0400

    Added api path binding for newGame and added newGame file

---
+++ server/controllers/newGame.js
@@ -0,0 +1,9 @@
+// Imports.
+const { insertGame } = require("../db/games.js");
+
+// Function to log a user in.
+const newGame = async (req, res) => {
+
+}
+
+module.exports = { newGame };
--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ app.use(cors());
 
 const { registerUser, verifyEmail } = require("./controllers/userRegistration.js");
 const { login } = require("./controllers/userLogin.js");
+const { newGame } = require("./controllers/newGame.js");
 
 app.post("/register", registerUser);
 app.get("/register/verifyEmail/:token", verifyEmail);
 app.post("/login", login);
-app.post("/games/create");
+app.post("/games/create", newGame);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
