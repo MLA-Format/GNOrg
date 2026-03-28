c41d8c9016a099d8dc31434042704c0327511f44
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-28 12:03:21 -0400

    Added logoff api as well as checking token denied list methods

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,16 +5,21 @@ const jwt = require("jsonwebtoken");
 const requireAuth = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    // Checking if Authorization header present in request.
+    // Checking if authorization header present in request.
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ error: "Unauthorized." });
     }
 
     const token = authHeader.split(" ")[1];
 
+    // Checking if the token has been denied.
+    if (tokenDenylist.has(token)) {
+        return res.status(401).json({ error: "Unauthorized." });
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // This sets req.user.id used in your controller
+        req.user = decoded; // Settubg req.user.id inside request.
         next();
     } catch (err) {
         return res.status(401).json({ error: "Unauthorized." });
