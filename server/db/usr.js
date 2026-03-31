90e242f261fc6a069649398ed89cb2a3eac1230d
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-03-31 16:36:18 -0400

    Added db password reset function

--- server/db/usr.js
+++ server/db/usr.js
@@ -25,8 +25,11 @@ const setUserVerified = async (id) => db().updateOne(
     { $set: { isVerified: true } }
 );
 
-// Function to store the JWT for the password reset.
-
+// Function to update a user's password.
+const updateUserPwd = async (id, hashedPassword) => db().updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { password: hashedPassword } }
+)
 
 
 module.exports = { connect, checkUserExistence, checkEmailExistence, findUserById, insertUser, setUserVerified };
