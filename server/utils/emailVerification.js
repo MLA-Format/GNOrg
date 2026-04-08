1418f1f49d3fbf54097d95a73c6013f3d26d9b14
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-04-08 19:56:34 -0400

    Email fix

--- server/utils/emailVerification.js
+++ server/utils/emailVerification.js
@@ -5,12 +5,16 @@ const { Resend } = require("resend");
 const sendEmail = async (options) => {
     const resend = new Resend(process.env.RESEND_APIKEY);
 
-    await resend.emails.send({
+    const { error } = await resend.emails.send({
         from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
         to: options.email,
         subject: options.subject,
         text: options.message,
     });
+
+    if (error) {
+        throw new Error(`Email send failed: ${error.message}`);
+    }
 }
 
 module.exports = { sendEmail };
